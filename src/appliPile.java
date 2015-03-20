aee6736a7e59e37b8375c7ba5d256028faa98bc2
ferdjoua <amine.ferdjoukh@iut-valence.fr>
2015-03-20 15:18:18 +0100

    Yé

--- src/appliPile.java
+++ src/appliPile.java
@@ -4,8 +4,11 @@ public class appliPile {
 	private static pile maPile;
 
 	public static void main(String[] args) {
-		maPile = new pile(7);
-		maPile.afficherPile(int pile[]);
+		
+		
+		
+		
+		
 		
 	}
 	
